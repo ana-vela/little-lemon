d2b8d2a50e609c4ee90f98f4e358f8b0703bc6e1
ana-vela <ana@anavela.net>
2024-03-31 09:30:40 -0600

    Adding new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Nav from "./components/Nav"
+import Header from "./components/Header"
+
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Header />
       Homepage
-    </div>
+    
+  <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Main() {
+    return (
+        <nav>
+            <p>Main</p>
+        
+        </nav>
+    )
+}
