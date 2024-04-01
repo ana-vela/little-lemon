9ddc733b85ee286b1b40d806204e9d19e45ac733
ana-vela <ana@anavela.net>
2024-03-31 22:19:17 -0600

    Adds hero content

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import heroImage from '../images/restaurantfood.jpg'
 
 export default function Main() {
     return (
         <>
-            <p>Main</p>
+        <div className="hero" >
+        <div className="hero-text">
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p> We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+        </div>
+        <div className="hero-image">
+        <img src={heroImage} alt="platter of food" />
+        </div>
+        </div>
         </>
     )
 }
