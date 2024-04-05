9a7a978880242e6403a0720c7f4701e4f57baa70
ana-vela <ana@anavela.net>
2024-04-05 00:16:09 -0600

    Adds basic footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import footerImage from '../images/logo-white.png'
 
 export default function Footer() {
     return (
         <>
-            <p>The Footer</p>
+            <div className="footer">
+                <div className="footer-logo">
+                    <img src={footerImage} alt="Little Lemon footer logo" />
+                </div>
+            <div className="footer-sitemap">
+                <h2>Doormat Navigation</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Menu</li>
+                        <li>Reservations</li>
+                        <li>Order Online</li>
+                        <li>Login</li>
+                    </ul>
+            </div>
+            <div className="footer-contact">
+                <h2>Contact Info</h2>
+                <p>987 Main St.</p>
+                <p>Los Angeles, CA</p>
+                <p>(213)456-7890</p>
+                <p>hello[at]littlelemon.com</p>
+            </div>
+            <div className="footer-social">
+                <p>Social Media Links</p>
+            </div>
+        </div>
         </>
     )
 }
