4acbd20c5ab974bdfd24856224405e5c46ce9c20
ana-vela <ana@anavela.net>
2024-04-04 22:24:07 -0600

    Adds about section content

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,82 +4,91 @@ import greekSalad from '../images/greek-salad.jpg'
 import lemonDessert from '../images/lemon-dessert.jpg'
 import bruschetta from '../images/bruchetta.svg'
 import testimonialPhoto from '../images/testimonial-photo.jpg'
+import aboutImage from '../images/Mario-and-Adrian.jpg'
 
 
 export default function Main() {
     return (
         <>
         <div className="hero">
-        <div className="hero-text">
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            <p> We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-        </div>
-        <div className="hero-image">
-        <img src={heroImage} alt="platter of food" />
-
-
-        </div>
+            <div className="hero-text">
+                <h1>Little Lemon</h1>
+                <h2>Chicago</h2>
+                <p> We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+            </div>
+            <div className="hero-image">
+                <img src={heroImage} alt="platter of food" />
+            </div>
         </div>
 
-< div className="menu">
-    <div className="menu-item">
-    <img className="item-image" src={greekSalad} alt="greek salad"  />
-    <div className="name-price">
-    <div className="item-name">Greek Salad</div>
-    <div className="item-price">$12.99</div>
-    </div>
-    <div className="item-description">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </div>
-    </div>
+        <div className="menu">
+            <div className="menu-item">
+                <img className="item-image" src={greekSalad} alt="greek salad"  />
+            <div className="name-price">
+                <div className="item-name">Greek Salad</div>
+                <div className="item-price">$12.99</div>
+            </div>
+            <div className="item-description">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </div>
+            </div>
 
-
-
-    <div className="menu-item">
-    <img className="item-image" src={bruschetta} alt="bruschetta"/>
-    <div className="name-price">
-    <div className="item-name">Bruschetta</div>
-    <div className="item-price">$5.99</div>
-    </div>
-    <div className="item-description">Our Bruschetta is made from grilled bread 
+            <div className="menu-item">
+                <img className="item-image" src={bruschetta} alt="bruschetta"/>
+            <div className="name-price">
+                <div className="item-name">Bruschetta</div>
+                <div className="item-price">$5.99</div>
+            </div>
+            <div className="item-description">Our Bruschetta is made from grilled bread 
     that has been smeared with garlic and seasoned with salt and olive oil.</div>
-    </div>
+            </div>
 
-    <div className="menu-item">
-    <img className="item-image" src={lemonDessert} alt="lemon dessert"  />
-    <div className="name-price">
-    <div className="item-name">Lemon Dessert</div>
-    <div className="item-price">$5.00</div>
-    </div>
-    <div className="item-description">This comes straight from grandma’s recipe book, 
+            <div className="menu-item">
+                <img className="item-image" src={lemonDessert} alt="lemon dessert"  />
+            <div className="name-price">
+                <div className="item-name">Lemon Dessert</div>
+                <div className="item-price">$5.00</div>
+            </div>
+            <div className="item-description">This comes straight from grandma’s recipe book, 
     every last ingredient has been sourced and is as authentic as can be imagined.</div>
-    </div>
-</div>
+            </div>
+        </div>
 
-<div className="testimonials-row">
-    <div className="testimonial">
-    <img className="testimonial-image" src={testimonialPhoto} alt="testimonial"  />
-    <div className="testimonial-name">Gina</div>
-    <div className="testimonial-text">hello there</div>
+        <div className="testimonials-row">
+            <div className="testimonial">
+            <img className="testimonial-image" src={testimonialPhoto} alt="testimonial"  />
+            <div className="testimonial-name">Gina</div>
+            <div className="testimonial-text">hello there</div>
+        </div>
+
+        <div className="testimonial">
+            <img className="testimonial-image" src={testimonialPhoto} alt="testimonial"  />
+            <div className="testimonial-name">Jane</div>
+            <div className="testimonial-text">hello there</div>
+        </div>
+
+        <div className="testimonial">
+            <img className="testimonial-image" src={testimonialPhoto} alt="testimonial"  />
+            <div className="testimonial-name">Laura</div>
+            <div className="testimonial-text">hello there</div>
+        </div>
+
+        <div className="testimonial">
+            <img className="testimonial-image" src={testimonialPhoto} alt="testimonial"  />
+            <div className="testimonial-name">Julia</div>
+            <div className="testimonial-text">hello there</div>
+        </div>
     </div>
 
-    <div className="testimonial">
-    <img className="testimonial-image" src={testimonialPhoto} alt="testimonial"  />
-    <div className="testimonial-name">Jane</div>
-    <div className="testimonial-text">hello there</div>
+    <div className="about">
+        <div className="about-text">
+            <h2 className="about-title">Little Lemon</h2>
+            <p className="about-location">Chicago</p>
+            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. </p>
+        </div>
+        <div>
+            <img className="about-image" src={aboutImage} alt="chefs in action"/>
+        </div>
     </div>
-
-    <div className="testimonial">
-    <img className="testimonial-image" src={testimonialPhoto} alt="testimonial"  />
-    <div className="testimonial-name">Laura</div>
-    <div className="testimonial-text">hello there</div>
-    </div>
-
-    <div className="testimonial">
-    <img className="testimonial-image" src={testimonialPhoto} alt="testimonial"  />
-    <div className="testimonial-name">Julia</div>
-    <div className="testimonial-text">hello there</div>
-    </div>
-</div>
 
         </>
     )
