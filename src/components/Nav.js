65fbc557e53c6da1aadf005035a0260946d0e824
ana-vela <ana@anavela.net>
2024-04-05 23:24:31 -0600

    Adjusts nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,14 +4,14 @@ import siteLogo from '../images/new-Logo.svg'
 export default function Navbar() {
     return (
         <nav>
-               <img src={siteLogo} alt="Little Lemon Logo" />
-               <ul>
-               <li>Home</li>
-               <li>About</li>
-               <li>Menu</li>
-               <li>Order Online</li>
-               <li>Log In</li>
-               </ul>
+            <img src={siteLogo} alt="Little Lemon Logo" />
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Order Online</li>
+                <li>Log In</li>
+            </ul>
         </nav>
     )
 }
